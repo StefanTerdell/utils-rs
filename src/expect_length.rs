d2caa5823aa138c5a123eece0b15e94ea154ce95
Stefan Terdell <stefan.terdell@cleura.com>
2025-11-06 10:34:36 +0100

    Autoimpl expect length for WithLen

--- src/expect_length.rs
+++ src/expect_length.rs
@@ -41,34 +41,47 @@ impl Display for ExpectedLenError {
     }
 }
 
-pub trait ExpectLen: WithLen {
-    fn expect_len_lt(&self, length: usize) -> Result<usize, ExpectedLenError> {
+pub trait ExpectLen: WithLen + Sized {
+    fn expect_len_lt(self, length: usize) -> Result<Self, ExpectedLenError>;
+    fn expect_len_lte(self, length: usize) -> Result<Self, ExpectedLenError>;
+    fn expect_len_eq(self, length: usize) -> Result<Self, ExpectedLenError>;
+    fn expect_len_gte(self, length: usize) -> Result<Self, ExpectedLenError>;
+    fn expect_len_gt(self, length: usize) -> Result<Self, ExpectedLenError>;
+}
+
+impl<T: WithLen + Sized> ExpectLen for T {
+    fn expect_len_lt(self, length: usize) -> Result<Self, ExpectedLenError> {
         Comparison::Less
             .expect_comparison(self.len(), length)
             .map_err(ExpectedLenError::from)
+            .map(|_| self)
     }
 
-    fn expect_len_lte(&self, length: usize) -> Result<usize, ExpectedLenError> {
+    fn expect_len_lte(self, length: usize) -> Result<Self, ExpectedLenError> {
         Comparison::LessOrEqual
             .expect_comparison(self.len(), length)
             .map_err(ExpectedLenError::from)
+            .map(|_| self)
     }
 
-    fn expect_len_eq(&self, length: usize) -> Result<usize, ExpectedLenError> {
+    fn expect_len_eq(self, length: usize) -> Result<Self, ExpectedLenError> {
         Comparison::Equal
             .expect_comparison(self.len(), length)
             .map_err(ExpectedLenError::from)
+            .map(|_| self)
     }
 
-    fn expect_len_gte(&self, length: usize) -> Result<usize, ExpectedLenError> {
+    fn expect_len_gte(self, length: usize) -> Result<Self, ExpectedLenError> {
         Comparison::GreaterOrEqual
             .expect_comparison(self.len(), length)
             .map_err(ExpectedLenError::from)
+            .map(|_| self)
     }
 
-    fn expect_len_gt(&self, length: usize) -> Result<usize, ExpectedLenError> {
+    fn expect_len_gt(self, length: usize) -> Result<Self, ExpectedLenError> {
         Comparison::Greater
             .expect_comparison(self.len(), length)
             .map_err(ExpectedLenError::from)
+            .map(|_| self)
     }
 }
