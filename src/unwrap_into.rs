de03cfb8283fd6eb00b55e0807a307d8c613b5f8
Stefan Terdell <stefan.terdell@cleura.com>
2025-11-07 14:54:01 +0100

    Add expect_into to UnwrapInto

--- src/unwrap_into.rs
+++ src/unwrap_into.rs
@@ -2,10 +2,15 @@ use std::fmt::Debug;
 
 pub trait UnwrapInto<T> {
     fn unwrap_into(self) -> T;
+    fn expect_into(self, message: &str) -> T;
 }
 
 impl<T, E: Debug, S: TryInto<T, Error = E>> UnwrapInto<T> for S {
     fn unwrap_into(self) -> T {
         self.try_into().unwrap()
+    }
+
+    fn expect_into(self, message: &str) -> T {
+        self.try_into().expect(message)
     }
 }
